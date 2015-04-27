f71aa3cc3e83addb5f69c4939a17096472aa3cc3
PHV1409 <phv@philiaweb.com>
2015-04-27 10:19:48 +0200

    20150427 récup config

---
+++ store/src/Store/BackendBundle/Controller/JewelerController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wac30
+ * Date: 27/04/15
+ * Time: 10:16
+ */
+
+namespace Store\BackendBundle\Controller;
+
+
+use Symfony\Component\HttpKernel\Tests\Controller;
+
+class JewelerController extends Controller{
+
+    public function myaccountAction(){
+
+
+    }
+} 
